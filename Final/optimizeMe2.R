1991fedf5804a238ff61154c8e97cd075535e49c
RWL <rlentz@gmail.com>
2017-12-18 11:57:00 -0600

    nightly

--- Final/optimizeMe2.R
+++ Final/optimizeMe2.R
@@ -1,4 +1,6 @@
-R = qr.Q(qr(matrix(c(1,2,3,4),nrow=2,ncol=2,byrow=T)))
+#install.packages('DEoptim')
+library('DEoptim')
+lR = qr.Q(qr(matrix(c(1,2,3,4),nrow=2,ncol=2,byrow=T)))
 
 
 fitness = function(x){
@@ -12,9 +14,9 @@ fitness = function(x){
 # on the domain -5 <= x <= 5, -5 <= y <= 5
 # you can call the function like this:
 # f( c(x,y) ), e.g.
-x1 = c(.5,.5);
-fitness(x1)
-
-
-
-
+#x1 = c(0,0);
+#fitness(x1);
+lower <- c(-5,-5);
+upper <- -lower;
+ans <- DEoptim(fitness, lower, upper,DEoptim.control(NP = 800,itermax = 40000, F = 1.2, CR = 0.7));
+#Iteration: 40000 bestvalit: 0.000000 bestmemit:   -0.632455   -0.316228
